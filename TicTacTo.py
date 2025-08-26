ff3c4688f92c115f6c8044636398a75114b444a4
CamCam5701 <cameronvoelpel@icloud.com>
2025-08-26 17:30:53 -0500

    updatinh the code

--- TicTacTo.py
+++ TicTacTo.py
@@ -1,5 +1,5 @@
-player1 = 'O'
-player2 = 'X'
+player1 = "O"
+player2 = "X"
 
 one_one = 0
 one_two = 0
@@ -31,7 +31,7 @@ print_board()
 
 game_active = True
 
-#while game_active:
+# while game_active:
 if player1Turn:
     player1input1 = input("It is player 1's turn, please enter the column number of an avalible space: ")
     player1input2 = input("please enter the row number of an avalible space: ")
@@ -64,3 +64,39 @@ if player1Turn:
             Player2Turn = True
             rounds += 1
             print_board()
+    elif player1inputs == (2, 2):
+        if two_two == 0:
+            two_two = player1
+            player1Turn = False
+            Player2Turn = True
+            rounds += 1
+            print_board()
+    elif player1inputs == (2, 3):
+        if two_three == 0:
+            two_three = player1
+            player1Turn = False
+            Player2Turn = True
+            rounds += 1
+            print_board()
+    elif player1inputs == (3, 1):
+        if three_one == 0:
+            three_one = player1
+            player1Turn = False
+            Player2Turn = True
+            rounds += 1
+            print_board()
+    elif player1inputs == (3, 2):
+        if three_two == 0:
+            three_two = player1
+            player1Turn = False
+            Player2Turn = True
+            rounds += 1
+            print_board()
+    elif player1inputs == (3, 3):
+        if three_three == 0:
+            three_three = player1
+            player1Turn = False
+            Player2Turn = True
+            rounds += 1
+            print_board()
+    
