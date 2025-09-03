08ecc4aea0c454a89e73818f6d52adf210e0a31a
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-09-03 14:00:41 -0700

    working on the code

--- Gambling/Gambling.py
+++ Gambling/Gambling.py
@@ -1,9 +1,21 @@
-def mainMenu():
-  print("""Welcome to Cams Casino, please select a game:
+mainMenuLoop = True
+game = False
+
+while mainMenuLoop:
+    def mainMenu():
+        print("""Welcome to Cams Casino, please select a game:
 Press 1 for Mines
 Press 2 for Coin Toss
 
 Version 0.0""")
-  gameSelction = input()
+        gameSelction = input()
+        if gameSelction == "1":
+            game = True
+            mainMenuLoop = False
+        elif gameSelction == "2":
+            game = True
+            mainMenuLoop = False
+        else:
+            print("INVALID INPUT")
 
 mainMenu()
