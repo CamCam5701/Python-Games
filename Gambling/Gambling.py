62f151636f9e5b05389035c8df0dfdd06b29a97e
CamCam5701 <cameronvoelpel@icloud.com>
2025-09-03 20:19:14 -0500

    fixing some stuff

--- Gambling/Gambling.py
+++ Gambling/Gambling.py
@@ -1,21 +1,23 @@
+money = 100.0
 mainMenuLoop = True
 game = False
 
-while mainMenuLoop:
-    def mainMenu():
-        print("""Welcome to Cams Casino, please select a game:
+def mainMenu():
+    global mainMenuLoop, game
+    print("""Welcome to Cams Casino, please select a game:
 Press 1 for Mines
 Press 2 for Coin Toss
 
 Version 0.0""")
-        gameSelction = input()
-        if gameSelction == "1":
-            game = True
-            mainMenuLoop = False
-        elif gameSelction == "2":
-            game = True
-            mainMenuLoop = False
-        else:
-            print("INVALID INPUT")
+    gameSelction = input()
+    if gameSelction == "1":
+        game = True
+        mainMenuLoop = False
+    elif gameSelction == "2":
+        game = True
+        mainMenuLoop = False
+    else:
+        print("INVALID INPUT")
 
-mainMenu()
+while mainMenuLoop:
+    mainMenu()
